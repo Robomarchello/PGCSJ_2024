2f85fcc45fd7731e4fdde7b23e26086ebe1bd56f
RoboMarchello <faccclol123@gmail.com>
2024-07-01 16:26:37 +0300

    new obstacle - asteroid + physics handling

--- src/engine/objects.py
+++ src/engine/objects.py
@@ -25,24 +25,96 @@ class BlackHole:
         return norm_vec, gravity_force
 
 
+class Asteroid:
+    def __init__(self, position, velocity, mass, radius):
+        self.position = pygame.Vector2(position)
+        self.velocity = pygame.Vector2(velocity)
+        self.acceleration = pygame.Vector2()
+        self.force = pygame.Vector2()
+
+        self.mass = mass
+        self.radius = radius
+
+        self.orientation = 0.0
+
+    def update(self, delta):
+        self.acceleration += self.force / self.mass
+        self.velocity += self.acceleration * delta * SPEED_FACTOR
+        self.position += self.velocity * delta * SPEED_FACTOR
+
+        self.acceleration *= 0
+        self.force *= 0
+    
+    def draw(self, surface):
+        pygame.draw.circle(surface, 'grey', self.position, self.radius)
+
+
+class ForceZone:
+    def __init__(self, force, rect, timer=0.0):
+        pass
+
+    def draw(self):
+        pass
+
+
+class GravityInvertor:
+    def __init__(self, position, timer, physics_handler):
+        self.position = position
+
+        self.timer = timer
+        self.crnt_timer = timer
+        self.physics_handler = physics_handler
+        
+    def draw(self, surface):
+        pass
+
+    def update(self, delta):
+        self.crnt_timer -= delta
+        if self.crnt_timer < 0:
+            self.crnt_timer = self.timer
+
+            # *invert here* (which is probably is *-1 object's or player's mass )
+
+
 class PhysicsHandler:
-    def __init__(self, player, objects):
+    def __init__(self, player, objects, obstacles):
         self.player = player
 
         self.objects = objects
+        self.obstacles = obstacles
 
     def draw(self, surface):
         for obj in self.objects:
             obj.draw(surface)
 
+        for obstacle in self.obstacles:
+            obstacle.draw(surface)
+
     def update(self, delta):
-        for object in self.objects:
-            if type(object) == BlackHole:
-                norm_vec, gravity_force = object.calculate_attraction(
+        for obj in self.objects:
+            if isinstance(obj, BlackHole):
+                norm_vec, gravity_force = obj.calculate_attraction(
                     self.player.position, self.player.mass
                 )
 
                 self.player.acceleration += norm_vec * gravity_force
+
+        self._update_obstacles(delta)
+            
+
+    def _update_obstacles(self, delta):
+        # update dynamic objects
+        for obstacle in self.obstacles:
+            for obj in self.objects:
+            
+                if isinstance(obj, BlackHole):
+                    norm_vec, gravity_force = obj.calculate_attraction(
+                        obstacle.position, obstacle.mass
+                    )
+
+                obstacle.force += norm_vec * gravity_force
+
+            obstacle.update(delta)
 
     def predict_player(self, time, position, start_vel, start_accel, count):
         '''
@@ -55,9 +127,9 @@ class PhysicsHandler:
 
         for _ in range(count):
             last_pos = positions[-1]
-            for object in self.objects:
-                if isinstance(object, BlackHole):
-                    norm_vec, gravity_force = object.calculate_attraction(
+            for obj in self.objects:
+                if isinstance(obj, BlackHole):
+                    norm_vec, gravity_force = obj.calculate_attraction(
                         last_pos, self.player.mass
                     )
 
--- src/states/game.py
+++ src/states/game.py
@@ -2,7 +2,7 @@ import pygame
 from src.engine import State, Debug, AssetManager
 from src.engine.constants import *
 from src.engine.player import Player, Controller
-from src.engine.objects import PhysicsHandler, BlackHole
+from src.engine.objects import PhysicsHandler, BlackHole, Asteroid
 
 
 class Game(State):
@@ -15,9 +15,12 @@ class Game(State):
         rect.center = self.player.position
         
         objects = []
-        objects.append(BlackHole((250, 384), 50))
+        #objects.append(BlackHole((250, 384), 50))
         objects.append(BlackHole((500, 384), 50))
-        self.physics_handler = PhysicsHandler(self.player, objects)
+        obstacles = []
+        obstacles.append(Asteroid((500, 280), pygame.Vector2(4.3, 0), 20, 15))
+        obstacles.append(Asteroid((500, 488), pygame.Vector2(-4.3, 0), 20, 15))
+        self.physics_handler = PhysicsHandler(self.player, objects, obstacles)
 
         self.controller = Controller(self.player, rect, self.physics_handler)
 
