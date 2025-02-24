2f9e7f8b00bd0162bdcfc1c25a8d7cdd69942d9f
RoboMarchello <faccclol123@gmail.com>
2025-02-24 21:24:33 +0200

    asteroid, black_hole, player inheritance and usage of object class; More inheritance to come!

--- src/engine/objects/asteroid.py
+++ src/engine/objects/asteroid.py
@@ -1,27 +1,20 @@
 import pygame
 from src.engine.constants import SPEED_FACTOR
-from src.engine.camera import Camera
 from src.engine.asset_manager import AssetManager
+from src.engine.objects.object import Object
 
 
-class Asteroid:
+class Asteroid(Object):
     def __init__(self, position, velocity, mass, radius):
-        self.position = pygame.Vector2(position)
-        self.velocity = pygame.Vector2(velocity)
-        self.acceleration = pygame.Vector2()
-        self.force = pygame.Vector2()
+        super().__init__(position, velocity, mass)
+        self.force = pygame.Vector2() # fix this? inconsistent with everything
 
         self.texture = AssetManager.images['asteroid']
         self.texture_rect = self.texture.get_rect()
 
-        self.mass = mass
         self.radius = radius
 
         self.orientation = 0.0
-
-    @property
-    def cam_pos(self):
-        return Camera.displace_position(self.position)
 
     def update(self, delta):
         self.acceleration += self.force / self.mass
--- src/engine/objects/black_hole.py
+++ src/engine/objects/black_hole.py
@@ -1,25 +1,24 @@
 import math
 import pygame
 from src.engine.constants import GRAVITY_CONST
-from src.engine.camera import Camera
 from src.engine.asset_manager import AssetManager
 from src.engine.vfx.emitters import BlackHoleEmitter
+from src.engine.objects.object import Object
 
 
-class BlackHole:
+class BlackHole(Object):
+    small_black_hole = AssetManager.images['smol_blek_hole']
+    black_hole_texture = AssetManager.images['black_hole']
+    small_white_hole = AssetManager.images['smol_white_hole']
+    white_hole_texture = AssetManager.images['white_hole']
+
     def __init__(self, position, mass):
-        self.position = pygame.Vector2(position)
-        self.mass = mass 
+        super().__init__(position, mass=mass)
 
-        self.radius = 30 + abs(mass) * 0.15
+        self.radius = 30 + abs(mass) * 0.15 # baaad
         self.color = pygame.Color(255, 255, 255)
 
-        self.small_black_hole = AssetManager.images['smol_blek_hole']
-        self.black_hole_texture = AssetManager.images['black_hole']
-
-        self.small_white_hole = AssetManager.images['smol_white_hole']
-        self.white_hole_texture = AssetManager.images['white_hole']
-
+        self.texture = self._determine_texture()
         self.texture_rect = pygame.Rect(0, 0, self.radius * 2, self.radius * 2)
 
         self.emitter = BlackHoleEmitter(self.position, self.radius, self.mass)
@@ -27,53 +26,44 @@ class BlackHole:
         self.pulsing_timer = 0.0
         self.pulsing = 1 + math.cos(self.pulsing_timer) * 0.1
 
-    @property
-    def cam_pos(self):
-        return Camera.displace_position(self.position)
-
     def draw(self, surface):
         scaled_rect = self.texture_rect.copy()
         scaled_rect.scale_by_ip(self.pulsing)
         scaled_rect.center = self.cam_pos
-        
-        if self.mass > 0:
-            if self.radius <= 64:
-                scaled_texture = pygame.transform.scale(
-                    self.small_black_hole, scaled_rect.size
-                )
-            else:
-                scaled_texture = pygame.transform.scale(
-                    self.black_hole_texture, scaled_rect.size
-                )
-                
-            #surface.blit(scaled_texture, scaled_rect.topleft)
-        elif self.mass < 0:
-            if self.radius <= 64:
-                scaled_texture = pygame.transform.scale(
-                    self.small_white_hole, scaled_rect.size
-                )
-            else:
-                scaled_texture = pygame.transform.scale(
-                    self.white_hole_texture, scaled_rect.size
-                )
-    
+
+        scaled_texture = pygame.transform.scale(self.texture, scaled_rect.size)  
+
         self.emitter.draw(surface)
         surface.blit(scaled_texture, scaled_rect.topleft)
 
+    def _determine_texture(self):
+        if self.mass > 0:
+            if self.radius <= 64:
+                return self.small_black_hole
+            else:
+                return self.black_hole_texture
+                
+        elif self.mass < 0:
+            if self.radius <= 64:
+                return self.small_white_hole
+            else:
+                return self.white_hole_texture
+
     def update(self, delta):
-        self.pulsing_timer += delta * 3
+        self.pulsing_timer += delta * 3 # meh constant
 
         self.pulsing = 1 + math.cos(self.pulsing_timer) * 0.1
 
         self.emitter.update_rect(self.position)
         self.emitter.update(delta)
 
-    def calculate_attraction(self, position_other, mass_other):
-        diff = self.position - position_other
+    def calculate_attraction(self, other_object: Object):
+        '''Calculate gravitation force between two objects'''
+        diff = self.position - other_object.position
         if diff == (0, 0):
             return pygame.Vector2()
         direction = diff.normalize()
         distance = diff.magnitude()
-        gravity_force = (GRAVITY_CONST * mass_other * self.mass) / distance ** 2
+        gravity_force = (GRAVITY_CONST * other_object.mass * self.mass) / distance ** 2
         
         return direction * gravity_force
