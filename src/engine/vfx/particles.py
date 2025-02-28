81b731fcb4cbe274c39881665f08366ce5e41db9
RoboMarchello <faccclol123@gmail.com>
2025-02-28 11:57:11 +0200

    Particle inherits object class

--- src/engine/vfx/particles.py
+++ src/engine/vfx/particles.py
@@ -1,20 +1,17 @@
 import pygame
 import math
-import random
-from src.engine.constants import SPEED_FACTOR
-from src.engine.camera import Camera
-from src.engine.utils import Debug
+from src.engine.objects import Object
 
 
-class Particle:
+class Particle(Object):
     def __init__(self, position, angle, speed, life, rotation,
                   rotation_change, color1, color2, texture):
-        self.position = position
-        self.velocity = pygame.Vector2(
+        velocity = pygame.Vector2(
             speed * math.cos(math.radians(angle)),
-            -speed * math.sin(math.radians(angle))
+            speed * -math.sin(math.radians(angle))
         )
-        self.acceleration = pygame.Vector2(0, 0)
+
+        super().__init__(position, velocity)
 
         self.life = life
         self.crnt_life = life
@@ -28,7 +25,3 @@ class Particle:
 
         self.texture = texture
         self.texture_rect = texture.get_rect()
-
-    @property
-    def cam_pos(self):
-        return Camera.displace_position(self.position)
