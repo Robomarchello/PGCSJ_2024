ab1902d1b06bfe27f125391bb9c89702c9a7e855
RoboMarchello <faccclol123@gmail.com>
2025-04-18 20:51:05 +0300

    Got wrong music, added intended one

--- src/states/game.py
+++ src/states/game.py
@@ -57,7 +57,7 @@ class Game(State):
             self.transition,
         ]
 
-        pygame.mixer.music.load('src/assets/sfx/music_3.mp3')
+        pygame.mixer.music.load('src/assets/sfx/music.mp3')
         pygame.mixer.music.set_volume(0.1)
         pygame.mixer.music.play(-1)
 
